2fad453d3e2eab93d87313bb537071b7bedb1281
yusufiwho <muhammedyusuf.h18@gmail.com>
2024-12-28 17:39:55 +0530

    final

--- askit/src/App.js
+++ askit/src/App.js
@@ -1,15 +1,13 @@
-import { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Navbar1 from './Component/Navbar-test/Navbar-test';
-import Navbar from './Component/Navbar/navbar';
+
 
 function App() {
 
-  const [showA,setshowA] = useState(true);
   return (
     <div> 
-      {showA?<Navbar/>:<Navbar1/>}
-      <button onClick={()=>setshowA(!showA)}>Toggle</button>
+      <Navbar1/>
     </div>
   );
 }
