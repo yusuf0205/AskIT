f119942df20451e08ba7a4b8f9787e6c039cbb56
yusufiwho <muhammedyusuf.h18@gmail.com>
2024-12-28 20:06:34 +0530

    finished header

--- askit/src/App.js
+++ askit/src/App.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Navbar1 from './Component/Navbar-test/Navbar-test';
 import NavbarWithPopup from './Component/Navbar-test/test';
 
-
 function App() {
-  const [showA,setShowA] = useState(true)
   return (
     <div> 
-      {showA?<Navbar1/>:<NavbarWithPopup/>}
-      <button onClick={() => setShowA(!showA)}>
-        Toggle Navbar
-      </button>
+      <NavbarWithPopup />
     </div>
   );
 }
